937417bec63957614438dbe77e37415a1a10f5fd
Candice Peters <candice.peters@flatironschool.com>
2021-06-29 11:49:12 -0400

    udated seed file

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,3 @@
 class Party < ApplicationRecord
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Party.create_or_find_by(name:"Halloween", date:Time.now, category:"scary", supplies: "Balloons and Things", private: false)
-Party.create_or_find_by(name:"Xmas Party", date:Time.now, category:"cheery", supplies: "snowman, candycane, and cookies", private: true)
+Party.find_or_create_by(name:"Halloween", date:Time.now, category:"scary", supplies: "Balloons and Things", private: false, budget: 500)
+Party.find_or_create_by(name:"Xmas Party", date:Time.now, category:"cheery", supplies: "snowman, candycane, and cookies", private: true, budget: 75)
+Party.find_or_create_by(name:"Dog Birthday", date:Time.now, category:"fun", supplies: "Dog cake, treats, and toys ", private: true, budget: 200)
+Party.find_or_create_by(name:"4th of July", date:Time.now, category:"fun", supplies: "grill, hotdogs, and hamburgers ", private: false, budget: 50)
