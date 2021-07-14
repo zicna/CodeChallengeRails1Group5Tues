cb0aed1f08b556b73be96cbda2bc635bfbd66faf
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-14 13:37:48 -0500

    create categories folder with index, show view and appropriate actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,24 @@
 class CategoriesController < ApplicationController
+    before_action :get_category, only: :show
+    before_action :get_categories, only: :index
+    def index
+    end
+
+    def show
+        @parties = @category.parties
+    end
+
+    private
+
+    def set_category
+        @category = Category.new
+    end
+
+    def get_category
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def get_categories
+        @categories = Category.all
+    end
 end
